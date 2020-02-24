d0dd201eb15857e6988aee2390a15d936b45d466
mandalpawan <pawankrmandal121@gmail.com>
2020-02-24 18:27:57 +0530

    P

--- showdata/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import applyleave
-from .models import leavetype
-from managementsystem.models import college
-
-# Register your models here.
-admin.site.register(applyleave),
-admin.site.register(leavetype),
-admin.site.register(college),
--- showdata/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShowdataConfig(AppConfig):
-    name = 'showdata'
